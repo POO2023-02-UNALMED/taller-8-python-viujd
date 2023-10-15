80bd4495e45910391f68da080c1a263b916a0030
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-10-15 15:57:29 -0500

    Create Futbolista.py

---
+++ Futbolista.py
@@ -0,0 +1,32 @@
+import Persona
+import Deportista
+class Futbolista (Persona, Deportista):
+    _listaFutbolista=[]
+    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(nombre, edad, altura, sexo)
+        Deportista.__init__(deporte, añosPracticando)
+        self._golesMarcados=golesMarcados
+        self._tarjetasRojas=tarjetasRojas
+        self._piernaHabil=piernaHabil
+        self._listaFutbolista.append(self)
+    
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados=golesMarcados
+
+    def getTarjetasRojas (self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas (self, tarjetasRojas):
+        self._tarjetasRojas=tarjetasRojas
+
+    def getPiernaHabil (self):
+        return self._piernaHabil
+    
+    def serPiernaHbail (self, piernaHabil):
+        self._piernaHabil=piernaHabil
+
+    def __str__(self):
+        return "Mi nombre es " + self.getNombre() + " soy profesional en el deporte " + self.getDeporte() + " Tengo " + str(self.getEdad()) + " años de edad y llevo " + str(self.getAñosPracticando()) + " años en el deporte"
